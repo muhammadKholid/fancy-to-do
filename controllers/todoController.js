08301c31237dac29c153d4193c8cb94884d36845
muhammadKholid <muhamma4dkhal3d@gmail.com>
2020-03-30 14:54:13 +0700

    requirement done

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -12,7 +12,7 @@ class Controller {
         if (err.errors[0].message) {
           res.status(400).json({ message: err.errors[0].message });
         } else {
-          res.status(500).json({ message: 'Server error' });
+          res.status(500).json({ message: 'Internal server error' });
         }
       });
   }
@@ -27,15 +27,55 @@ class Controller {
       });
   }
 
-  //static editTodo(req, res) {
-  //  //
-  //  Todo.update({ where: { id: id } });
-  //}
+  static getSpecificTodo(req, res) {
+    const id = req.params.id;
+    Todo.findOne({ where: { id: id } })
+      .then((data) => {
+        if (data) {
+          res.status(200).json({ data });
+        } else {
+          res.status(404).json({ message: 'Not found' });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({ message: 'Inyernal server error' });
+      });
+  }
 
-  //static deleteTodo(req, res) {
-  //  //
-  //  Todo.destroy({ where: { id: id } });
-  //}
+  static editTodo(req, res) {
+    const editedData = req.body;
+    const id = req.params.id;
+    Todo.update(editedData, { where: { id: id } })
+      .then((data) => {
+        if (data == 0) {
+          res.status(404).json({ message: 'Not found' });
+        } else {
+          res.status(200).json({ message: 'Berhasil mengubah data' });
+        }
+      })
+      .catch((err) => {
+        if (err.errors[0].message) {
+          res.status(400).json({ message: err.errors[0].message });
+        } else {
+          res.status(500).json({ message: 'Internal server error' });
+        }
+      });
+  }
+
+  static deleteTodo(req, res) {
+    const id = req.params.id;
+    Todo.destroy({ where: { id: id } })
+      .then((data) => {
+        if (data == 0) {
+          res.status(404).json({ message: 'Not found' });
+        } else {
+          res.status(200).json({ message: 'Berhasil menghapus data' });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({ message: 'Internal server error' });
+      });
+  }
 }
 
 module.exports = Controller;
--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -7,6 +7,12 @@ const router = express.Router();
 router.post('/', todoController.addTodo);
 //get all todo list
 router.get('/', todoController.getTodo);
+//get list based on the id
+router.get('/:id', todoController.getSpecificTodo);
+//update data
+router.put('/:id', todoController.editTodo);
+//delete data
+router.delete('/:id', todoController.deleteTodo);
 
 module.exports = router;
 
