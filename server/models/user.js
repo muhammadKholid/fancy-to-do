c90097d76a4c5118b2b19e1f36f70195837ce295
muhammadKholid <muhamma4dkhal3d@gmail.com>
2020-03-31 15:48:10 +0700

    make validation in user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcrypt');
 module.exports = (sequelize, DataTypes) => {
   const Sequelize = sequelize.Sequelize;
   const Model = Sequelize.Model;
@@ -9,11 +10,56 @@ module.exports = (sequelize, DataTypes) => {
 
   User.init(
     {
-      username: DataTypes.STRING,
-      password: DataTypes.STRING,
-      email: DataTypes.STRING,
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: 'Username tidak boleh kosong',
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          min: {
+            args: 6,
+            msg: 'Password harus lebih dari 6 character',
+          },
+          notEmpty: {
+            msg: 'Password tidak boleh kosong',
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+          notEmpty: {
+            msg: 'Email tidak boleh kosong',
+          },
+        },
+      },
     },
-    { sequelize }
+    {
+      sequelize,
+      hooks: {
+        beforeCreate: (User, options) => {
+          if (User.password) {
+            bcrypt
+              .hash(User.password, 10)
+              .then((hashed) => {
+                User.password = hashed;
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }
+        },
+      },
+    }
   );
   User.associate = function (models) {
     // associations can be defined here
@@ -21,4 +67,3 @@ module.exports = (sequelize, DataTypes) => {
   };
   return User;
 };
-
