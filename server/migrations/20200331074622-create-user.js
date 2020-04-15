01692c6b0d0b2153c6cd077f5c3ab753c4abfc74
muhammadKholid <muhamma4dkhal3d@gmail.com>
2020-04-15 14:29:04 +0700

    constraint email unique dan username

--- server/migrations/20200331074622-create-user.js
+++ server/migrations/20200331074622-create-user.js
@@ -1,33 +1,45 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      username: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+    return queryInterface
+      .createTable('Users', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        username: {
+          type: Sequelize.STRING,
+        },
+        password: {
+          type: Sequelize.STRING,
+        },
+        email: {
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      })
+      .then(() =>
+        queryInterface.addConstraint('Users', ['email'], {
+          type: 'unique',
+        })
+      )
+      .then(() =>
+        queryInterface.addConstraint('Users', ['username'], {
+          type: 'unique',
+        })
+      );
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Users');
-  }
+  },
 };
+
